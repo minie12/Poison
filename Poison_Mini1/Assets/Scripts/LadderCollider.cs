bf46c0a8d273cbeaace4315854447a73bc5c9e19
minie12 <30361753+minie12@users.noreply.github.com>
2018-07-19 17:24:16 +0900

    'init'

---
+++ Poison_Mini1/Assets/Scripts/LadderCollider.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadderCollider : MonoBehaviour {
+
+    private BoxCollider2D boxCollider;
+        
+	// Use this for initialization
+	void Start () {
+        boxCollider = GetComponent<BoxCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(boxCollider.isTrigger);
+	}
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.tag == "GroundCheck" && Input.GetKey("down"))
+        {
+            boxCollider.isTrigger = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "GroundCheck" &&Input.GetKey("up"))
+        {
+            boxCollider.isTrigger = false;
+        }
+    }
+
+}
---
+++ Poison_Mini1/Assets/flogmovement.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flogmovement : MonoBehaviour
+{
+
+    public float Charc_Speed;
+    public Transform groundCheck; //to check if player is touching ground
+    public float jumpForce = 100f; //jump height
+    public LayerMask whatIsGround;
+
+    bool facingRight = true; //shifting image left & right
+    bool grounded = false; // for detecting ground
+    bool onLadder = false; //for ladder
+
+    private float climbSpeed = 0.6f; //speed when climbing up the ladder
+    float groundRadius = 0.01f; //distance from player to ground (when grounded)
+
+    Animator anim;
+    private Rigidbody2D rgbd;
+
+    // Use this for initialization
+    void Start()
+    {
+        rgbd = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (grounded && Input.GetButtonDown("Jump"))
+        {
+            anim.SetBool("jump", true);
+            rgbd.AddForce(new Vector2(0, jumpForce));
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+
+        anim.SetBool("jump", !grounded);
+        anim.SetFloat("vSpeed", rgbd.velocity.y); //how fast we are moving up n down
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        rgbd.velocity = new Vector2(horizontal * Charc_Speed, rgbd.velocity.y);
+
+        anim.SetFloat("walk", Mathf.Abs(horizontal));
+        anim.SetFloat("climb", Mathf.Abs(vertical));
+
+        if (horizontal > 0 && !facingRight)
+            Flip();
+        else if (horizontal < 0 && facingRight)
+            Flip();
+
+        if (onLadder)
+        {
+            rgbd.velocity = new Vector2(0, vertical * climbSpeed);
+            rgbd.gravityScale = 0;
+            if (Input.GetButton("Horizontal") && Input.GetButtonDown("Jump"))
+            {
+                onLadder = false;
+                anim.SetBool("ladder", false);
+                rgbd.gravityScale = 0.9f;
+            }
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+
+        Vector3 theScale = transform.localScale;
+
+        theScale.x *= -1;
+
+        transform.localScale = theScale;
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Ladder" && Input.GetButton("Vertical"))
+        {
+            onLadder = true;
+            anim.SetBool("ladder", true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Ladder")
+        {
+            onLadder = false;
+            anim.SetBool("ladder", false);
+            rgbd.gravityScale = 0.9f;
+        }
+    }
+}
